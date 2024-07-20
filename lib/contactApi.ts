8696651b9bd9516f2f86eeb61abb8cd47c51e122
harshWebbuddy <165643599+harshWebbuddy@users.noreply.github.com>
2024-07-19 23:55:18 -0700

    error

--- lib/contactApi.ts
+++ lib/contactApi.ts
@@ -1,9 +1,9 @@
-export const sendContactForm = (url, data) =>
-  fetch(`/api/contact/${url}/`, {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "multipart/form-data",
-      Accept: "application/json",
-    },
-  });
+// export const sendContactForm = (url, data) =>
+//   fetch(`/api/contact/${url}/`, {
+//     method: "POST",
+//     body: JSON.stringify(data),
+//     headers: {
+//       "Content-Type": "multipart/form-data",
+//       Accept: "application/json",
+//     },
+//   });
