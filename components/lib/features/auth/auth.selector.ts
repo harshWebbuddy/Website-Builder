f01d220715dd15cdb09c378dc57e260d4f49b35e
harsh9711 <harsh@webbuddy.agency>
2024-07-23 21:02:37 +0530

    store commented

--- components/lib/features/auth/auth.selector.ts
+++ components/lib/features/auth/auth.selector.ts
@@ -1,13 +1,13 @@
-// useAuth.ts
-import { useSelector } from "react-redux";
-import { RootState } from "../../../../components/lib/store";
+// // useAuth.ts
+// import { useSelector } from "react-redux";
+// import { RootState } from "../../../../components/lib/store";
 
-const IsAuthenticated = () => {
-  return useSelector((state: RootState) => state.auth.isAuthenticated);
-};
+// const IsAuthenticated = () => {
+//   return useSelector((state: RootState) => state.auth.isAuthenticated);
+// };
 
-const GetCurrentUser = () => {
-  return useSelector((state: RootState) => state.auth.user);
-};
+// const GetCurrentUser = () => {
+//   return useSelector((state: RootState) => state.auth.user);
+// };
 
-export { IsAuthenticated, GetCurrentUser };
+// export { IsAuthenticated, GetCurrentUser };
