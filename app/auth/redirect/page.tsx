9de30f1ce3a34601bf982ae9a3b467097dcef9ad
harshWebbuddy <165643599+harshWebbuddy@users.noreply.github.com>
2024-07-23 22:06:35 -0700

    login error

--- app/auth/redirect/page.tsx
+++ app/auth/redirect/page.tsx
@@ -1,54 +1,54 @@
-'use client';
+// 'use client';
 
-import Spinner from "@/components/Spinner";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-import toast from "react-hot-toast";
-import { useDispatch } from "react-redux";
-import { login } from "../../../components/lib/features/auth/auth.slice";
-import { setCookie } from "cookies-next";
-import { useSearchParams } from "next/navigation";
+// import Spinner from "@/components/Spinner";
+// import { useRouter } from "next/navigation";
+// import { useEffect } from "react";
+// import toast from "react-hot-toast";
+// import { useDispatch } from "react-redux";
+// import { login } from "../../../components/lib/features/auth/auth.slice";
+// import { setCookie } from "cookies-next";
+// import { useSearchParams } from "next/navigation";
 
-export default function Login() {
-  const router = useRouter();
-  const dispatch = useDispatch();
-  const searchParams = useSearchParams();
+// export default function Login() {
+//   const router = useRouter();
+//   const dispatch = useDispatch();
+//   const searchParams = useSearchParams();
   
-  useEffect(() => {
-    const token = searchParams.get("token");
+//   useEffect(() => {
+//     const token = searchParams.get("token");
 
-    if (token) {
-      const user_name = searchParams.get("user_name");
-      const id = searchParams.get("id");
-      const email = searchParams.get("email");
-      const avatar = searchParams.get("avatar");
-      setCookie("token", token, {
-        secure: true,
-        sameSite: "none",
-        expires: new Date(Date.now() + 2 * 60 * 60 * 1000),
-      });
-      const user = {
-        user_name,
-        email,
-        id,
-        avatar,
-      };
-      dispatch(login(user));
+//     if (token) {
+//       const user_name = searchParams.get("user_name");
+//       const id = searchParams.get("id");
+//       const email = searchParams.get("email");
+//       const avatar = searchParams.get("avatar");
+//       setCookie("token", token, {
+//         secure: true,
+//         sameSite: "none",
+//         expires: new Date(Date.now() + 2 * 60 * 60 * 1000),
+//       });
+//       const user = {
+//         user_name,
+//         email,
+//         id,
+//         avatar,
+//       };
+//       dispatch(login(user));
 
-      router.push("/mainapp");
-      toast.success("Successfully logged in...");
-    } else {
-      router.push("/auth"); //login commented
-    }
-  }, [searchParams, dispatch, router]);
+//       router.push("/mainapp");
+//       toast.success("Successfully logged in...");
+//     } else {
+//       router.push("/auth"); //login commented
+//     }
+//   }, [searchParams, dispatch, router]);
 
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h2>Wait Redirecting to dashboard...</h2>
-      <div className="itemh-screen">
-        <div className="w-12 h-12 rounded-full absolute border-8 border-solid border-gray-200"></div>
-        <div className="w-12 h-12 rounded-full animate-spin absolute border-8 border-solid border-green-500 border-t-transparent"></div>
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="flex flex-col items-center justify-center h-screen">
+//       <h2>Wait Redirecting to dashboard...</h2>
+//       <div className="itemh-screen">
+//         <div className="w-12 h-12 rounded-full absolute border-8 border-solid border-gray-200"></div>
+//         <div className="w-12 h-12 rounded-full animate-spin absolute border-8 border-solid border-green-500 border-t-transparent"></div>
+//       </div>
+//     </div>
+//   );
+// }
