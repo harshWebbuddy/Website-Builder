053d484323ce61b075ac11e45e6dce4e208d43db
harshWebbuddy <165643599+harshWebbuddy@users.noreply.github.com>
2024-07-24 06:11:58 -0700

    website building

--- app/websitebuilding/view/page.tsx
+++ app/websitebuilding/view/page.tsx
@@ -1,27 +1,37 @@
 "use client";
 
-
 import { Eye, Rocket } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState, useRef } from "react";
-import { SlScreenDesktop, SlScreenTablet, SlScreenSmartphone } from "react-icons/sl";
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import {
+  SlScreenDesktop,
+  SlScreenTablet,
+  SlScreenSmartphone,
+} from "react-icons/sl";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import clsx from "clsx";
-// import { useGeneratedHtml } from "@/components/context/GeneratedHtmlContext";
 import { Switch } from "@/components/ui/switch";
-// import { WwwIcon } from "@/components/svgs";
+import RootLayout from "@/app/layout";
+
 export default function ViewGeneratedWebsite() {
-  // const { generatedHtml } = useGeneratedHtml();
   const router = useRouter();
   const [iframeWidth, setIframeWidth] = useState("100%");
-  const [editingMode, setEditingMode] = useState(false); // State to track editing mode
+  const [editingMode, setEditingMode] = useState(false);
+  const [generatedHtml, setGeneratedHtml] = useState<string | null>(null);
   const iframeRef = useRef<HTMLIFrameElement>(null);
 
-  // useEffect(() => {
-  //   if (!generatedHtml) {
-  //     router.push("/websitebuilding"); // Redirect if no HTML is generated
-  //   }
-  // }, [generatedHtml, router]);
+  useEffect(() => {
+    const url = new URL(window.location.href);
+    const content = url.searchParams.get("doc_content");
+    if (content) {
+      setGeneratedHtml(decodeURIComponent(content));
+    }
+  }, [router]);
 
   const handleViewScreenChange = (view: string) => {
     switch (view) {
@@ -45,9 +55,9 @@ export default function ViewGeneratedWebsite() {
       const iframeDocument = iframeRef.current.contentDocument;
       if (iframeDocument) {
         iframeDocument.querySelectorAll<HTMLElement>("*").forEach((element) => {
-          element.contentEditable = editingMode ? "false" : "true"; // Toggle content editable
+          element.contentEditable = editingMode ? "false" : "true";
           if (editingMode) {
-            element.style.outline = "none"; // Remove outline on edit disable
+            element.style.outline = "none";
           } else {
             element.addEventListener("mouseover", handleMouseOver);
             element.addEventListener("mouseout", handleMouseOut);
@@ -57,7 +67,7 @@ export default function ViewGeneratedWebsite() {
         });
       }
     }
-    setEditingMode(!editingMode); // Toggle editing mode state
+    setEditingMode(!editingMode);
   };
 
   const handleMouseOver = (event: MouseEvent) => {
@@ -81,98 +91,109 @@ export default function ViewGeneratedWebsite() {
   };
 
   return (
-    <main className="flex-1 h-full w-full flex flex-col mt-10">
-      <div className="bg-white py-3 px-5 flex justify-between items-center rounded-xl translate-y-1 shadow shadow-gray-100">
-        <div className="flex items-center gap-x-1.5 w-full max-w-fit">
-          <span className="w-3 h-3 rounded-full bg-[#EF4444]" />
-          <span className="w-3 h-3 rounded-full bg-[#EAB308]" />
-          <span className="w-3 h-3 rounded-full bg-[#22C55E]" />
-        </div>
-
-        <div className="flex items-center gap-5">
-          <div className="flex items-center gap-2">
-            <Switch checked={editingMode} onClick={handleEdit} />
-            Construction Mode
+    <RootLayout shoWebar={true}>
+      <main className="flex-1 h-full w-full flex flex-col mt-10">
+        <div className="bg-white py-3 px-5 flex justify-between max-w-[1668px] mx-auto w-full items-center rounded-xl translate-y-1 shadow shadow-gray-100 mt-20">
+          <div className="flex items-center gap-x-1.5 w-full max-w-fit">
+            <span className="w-3 h-3 rounded-full bg-[#EF4444]" />
+            <span className="w-3 h-3 rounded-full bg-[#EAB308]" />
+            <span className="w-3 h-3 rounded-full bg-[#22C55E]" />
           </div>
-          <div className="flex items-center gap-3">
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <button
-                    className={clsx(
-                      "border h-10 w-10 rounded-lg flex gap-2 items-center justify-center",
-                      iframeWidth === "100%" && "border-[3px] border-blue-400"
-                    )}
-                    onClick={() => handleViewScreenChange("desktop")}>
-                    <SlScreenDesktop size={22} />
-                  </button>
-                </TooltipTrigger>
-                <TooltipContent className="bg-white">
-                  <p>View in desktop</p>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <button
-                    className={clsx(
-                      "border h-10 w-10 rounded-lg flex gap-2 items-center justify-center",
-                      iframeWidth === "768px" && "border-[3px] border-blue-400"
-                    )}
-                    onClick={() => handleViewScreenChange("tablet")}>
-                    <SlScreenTablet size={22} />
-                  </button>
-                </TooltipTrigger>
-                <TooltipContent className="bg-white">
-                  <p>View in tablet</p>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <button
-                    className={clsx(
-                      "border h-10 w-10 rounded-lg flex gap-2 items-center justify-center",
-                      iframeWidth === "375px" && "border-[3px] border-blue-400"
-                    )}
-                    onClick={() => handleViewScreenChange("mobile")}>
-                    <SlScreenSmartphone size={22} />
-                  </button>
-                </TooltipTrigger>
-                <TooltipContent className="bg-white">
-                  <p>View in mobile</p>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
 
-            <button className="border h-10 w-10 rounded-lg flex gap-2 items-center justify-center">
-              {/* <WwwIcon /> */}wwicon
-            </button>
-            <button className="border h-10 py-2 px-4 rounded-lg flex gap-2 items-center justify-center">
-              <Rocket size={20} />
-              Publish
-            </button>
-            <button className="border h-10 py-2 px-4 rounded-lg flex gap-2 items-center justify-center">
-              <Eye size={20} />
-              Preview
-            </button>
+          <div className="flex items-center gap-5">
+            <div className="flex items-center gap-2">
+              <Switch checked={editingMode} onClick={handleEdit} />
+              Construction Mode
+            </div>
+            <div className="flex items-center gap-3">
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <button
+                      className={clsx(
+                        "border h-10 w-10 rounded-lg flex gap-2 items-center justify-center",
+                        iframeWidth === "100%" && "border-[3px] border-blue-400"
+                      )}
+                      onClick={() => handleViewScreenChange("desktop")}
+                    >
+                      <SlScreenDesktop size={22} />
+                    </button>
+                  </TooltipTrigger>
+                  <TooltipContent className="bg-white">
+                    <p>View in desktop</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <button
+                      className={clsx(
+                        "border h-10 w-10 rounded-lg flex gap-2 items-center justify-center",
+                        iframeWidth === "768px" &&
+                          "border-[3px] border-blue-400"
+                      )}
+                      onClick={() => handleViewScreenChange("tablet")}
+                    >
+                      <SlScreenTablet size={22} />
+                    </button>
+                  </TooltipTrigger>
+                  <TooltipContent className="bg-white">
+                    <p>View in tablet</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <button
+                      className={clsx(
+                        "border h-10 w-10 rounded-lg flex gap-2 items-center justify-center",
+                        iframeWidth === "375px" &&
+                          "border-[3px] border-blue-400"
+                      )}
+                      onClick={() => handleViewScreenChange("mobile")}
+                    >
+                      <SlScreenSmartphone size={22} />
+                    </button>
+                  </TooltipTrigger>
+                  <TooltipContent className="bg-white">
+                    <p>View in mobile</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+
+              <button className="border h-10 w-10 rounded-lg flex gap-2 items-center justify-center">
+                {/* <WwwIcon /> */}wwicon
+              </button>
+              <button className="border h-10 py-2 px-4 rounded-lg flex gap-2 items-center justify-center">
+                <Rocket size={20} />
+                Publish
+              </button>
+              <button className="border h-10 py-2 px-4 rounded-lg flex gap-2 items-center justify-center">
+                <Eye size={20} />
+                Preview
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-      {/* <div className="flex-1">
-        {generatedHtml ? (
-          <iframe
-            ref={iframeRef}
-            srcDoc={generatedHtml}
-            className="w-full h-full pt-3 bg-white"
-            style={{ width: iframeWidth, height: "calc(100vh - 100px)", margin: "0 auto" }} // Adjust width and center the iframe
-          />
-        ) : (
-          <p className="text-center text-gray-500">Loading...</p>
-        )}
-      </div> */}
-    </main>
+        <div className="flex-1 max-w-[1668px]  w-full mx-auto items-center justify-center">
+          {generatedHtml ? (
+            <iframe
+              ref={iframeRef}
+              srcDoc={generatedHtml}
+              className="w-full h-full pt-3 bg-white"
+              style={{
+                width: iframeWidth,
+                height: "calc(100vh - 100px)",
+                margin: "0 auto",
+              }} 
+            />
+          ) : (
+            <p className="text-center text-gray-500">Loading...</p>
+          )}
+        </div>
+      </main>
+    </RootLayout>
   );
 }
