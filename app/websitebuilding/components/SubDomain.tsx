8e2ad1b30927e35f2ac1147977faae3c00670470
harshWebbuddy <165643599+harshWebbuddy@users.noreply.github.com>
2024-07-26 05:35:46 -0700

    error fixing

--- app/websitebuilding/components/SubDomain.tsx
+++ app/websitebuilding/components/SubDomain.tsx
@@ -8,38 +8,38 @@ const SubDomain = () => {
           Subdomain configuration
         </h2>
         <p className="2xl:text-[12px] xl:text-[10px] text-[8px] font-light  ">
-          Subdomains are prefixes to your project's URL. You can use them for
+          Subdomains are prefixes to your projects URL. You can use them for
           public access or testing.
         </p>
       </div>
       <div className="flex flex-col gap-1.5 font-medium">
-        {" "}
+      
         <h2>
-          Draft subdomain{" "}
+          Draft subdomain
           <span className="text-red-500 font-bold xl:text-[14px] sm:text-[12px]">
             *
           </span>
         </h2>
         <span className="justify-between rounded-xl font-light w-full flex bg-[#F2F2F2]  flex-row">
           <button className="UPPERCASE   text-black text-[14px] font-semibold rounded-xl p-2 px-4">
-            aibuilder.blythe-jbe{" "}
-          </button>{" "}
+            aibuilder.blythe-jbe
+          </button>
           <button className="UPPERCASE bg-[#E5E5E5] w-full max-w-[147px] text-black text-[14px]  rounded-xl p-2 px-4">
-            .aibuilder.dev{" "}
+            .aibuilder.dev
           </button>
         </span>
       </div>
       <div className="flex flex-col gap-1.5 font-medium">
-        {" "}
+   
         <h2>
-          Draft subdomain{" "}
+          Draft subdomain
           <span className="text-red-500 font-bold xl:text-[14px] sm:text-[12px]">
             *
           </span>
         </h2>
         <span className="justify-between rounded-xl font-light w-full flex bg-[#F2F2F2]  flex-row">
           <button className="UPPERCASE   text-black text-[14px] font-semibold rounded-xl p-2 px-4">
-            aibuilder.blythe-jbe{" "}
+            aibuilder.blythe-jbe
           </button>{" "}
           <button className="UPPERCASE bg-[#E5E5E5] w-full max-w-[147px] text-black text-[14px]  rounded-xl p-2 px-4">
             .aibuilder.dev{" "}
